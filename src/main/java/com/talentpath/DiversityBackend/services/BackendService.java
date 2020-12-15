3b9ca57899b068ba4558dd76f1df0fc50cc7ca9f
Chad Wiesen <cwiesen@talentpath.com>
2020-12-15 11:12:42 -0600

    Added governor file and script to upload to database

--- src/main/java/com/talentpath/DiversityBackend/services/BackendService.java
+++ src/main/java/com/talentpath/DiversityBackend/services/BackendService.java
@@ -24,7 +24,7 @@ public class BackendService {
     public void readFile() {
         List<String> fileRows = new ArrayList<>();
         try {
-            File fileData = new File("personData.csv");
+            File fileData = new File("Governors.csv");
             Scanner myReader = new Scanner(fileData);
             while (myReader.hasNextLine()) {
                 fileRows.add(myReader.nextLine());
@@ -34,10 +34,33 @@ public class BackendService {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        for (String line : fileRows) {
+        for (int i = 1; i < fileRows.size(); i++) {
+            String line = fileRows.get(i);
+            line.replace("\"", "");
             String[] currentLine = line.split(",");
-            System.out.println(currentLine.length);
+            if (currentLine.length < 8) {
+                System.out.println("Bad index");
+            }
+            dao.addPerson(buildGovernor(currentLine));
         }
+    }
+
+    public Person buildGovernor(String[] toAdd) {
+        Person newPerson = new Person();
+        String state = convertState(toAdd[0]);
+
+        newPerson.setStateName(state);
+        newPerson.setAge(Integer.parseInt(toAdd[8]));
+        newPerson.setGender(toAdd[6]);
+        newPerson.setEthnicity(toAdd[5]);
+        newPerson.setParty(toAdd[7]);
+        newPerson.setPosition("Governor");
+        newPerson.setStartYear(convertDate(toAdd[1]));
+        newPerson.setEndYear(null);
+        if (!toAdd[2].isEmpty()) {
+            newPerson.setEndYear(convertDate(toAdd[2]));
+        }
+        return newPerson;
     }
 
     public void addPerson() {
@@ -47,5 +70,170 @@ public class BackendService {
 
     public List<Person> getAllPeople() {
         return dao.getAllPeople();
+    }
+
+    private Integer convertDate(String stringDate) {
+        String[] splitDate = stringDate.split("/");
+        return Integer.parseInt(splitDate[2]);
+    }
+
+    private String convertState(String stateId) {
+        String state;
+        switch (stateId) {
+            case "1":
+                state = "Alabama";
+                break;
+            case "2":
+                state = "Alaska";
+                break;
+            case "3":
+                state = "Arizona";
+                break;
+            case "4":
+                state = "Arkansas";
+                break;
+            case "5":
+                state = "California";
+                break;
+            case "6":
+                state = "Colorado";
+                break;
+            case "7":
+                state = "Connecticut";
+                break;
+            case "8":
+                state = "Delaware";
+                break;
+            case "9":
+                state = "Florida";
+                break;
+            case "10":
+                state = "Georgia";
+                break;
+            case "11":
+                state = "Hawaii";
+                break;
+            case "12":
+                state = "Idaho";
+                break;
+            case "13":
+                state = "Illinois";
+                break;
+            case "14":
+                state = "Indiana";
+                break;
+            case "15":
+                state = "Iowa";
+                break;
+            case "16":
+                state = "Kansas";
+                break;
+            case "17":
+                state = "Kentucky";
+                break;
+            case "18":
+                state = "Louisiana";
+                break;
+            case "19":
+                state = "Maine";
+                break;
+            case "20":
+                state = "Maryland";
+                break;
+            case "21":
+                state = "Massachusetts";
+                break;
+            case "22":
+                state = "Michigan";
+                break;
+            case "23":
+                state = "Minnesota";
+                break;
+            case "24":
+                state = "Mississippi";
+                break;
+            case "25":
+                state = "Missouri";
+                break;
+            case "26":
+                state = "Montana";
+                break;
+            case "27":
+                state = "Nebraska";
+                break;
+            case "28":
+                state = "Nevada";
+                break;
+            case "29":
+                state = "New Hampshire";
+                break;
+            case "30":
+                state = "New Jersey";
+                break;
+            case "31":
+                state = "New Mexico";
+                break;
+            case "32":
+                state = "New York";
+                break;
+            case "33":
+                state = "North Carolina";
+                break;
+            case "34":
+                state = "North Dakota";
+                break;
+            case "35":
+                state = "Ohio";
+                break;
+            case "36":
+                state = "Oklahoma";
+                break;
+            case "37":
+                state = "Oregon";
+                break;
+            case "38":
+                state = "Pennsylvania";
+                break;
+            case "39":
+                state = "Rhode Island";
+                break;
+            case "40":
+                state = "South Carolina";
+                break;
+            case "41":
+                state = "South Dakota";
+                break;
+            case "42":
+                state = "Tennessee";
+                break;
+            case "43":
+                state = "Texas";
+                break;
+            case "44":
+                state = "Utah";
+                break;
+            case "45":
+                state = "Vermont";
+                break;
+            case "46":
+                state = "Virginia";
+                break;
+            case "47":
+                state = "Washington";
+                break;
+            case "48":
+                state = "West Virginia";
+                break;
+            case "49":
+                state = "Wisconsin";
+                break;
+            case "50":
+                state = "Wisconsin";
+                break;
+            default:
+                state = "";
+                break;
+        }
+        return state;
     }
 }
